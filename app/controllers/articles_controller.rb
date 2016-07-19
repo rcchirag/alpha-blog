fb8ae5d418a71aaa27ef14f4ca4a29c395635a7b
chirag <chichiveechi5@gmail.com>
2016-07-19 09:20:14 +0000

    Editing the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,40 @@
 class ArticlesController < ApplicationController
 
   def new
-    @article = Article.new
+    @articles = Article.new
   end
 
   def create
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+      @articles = Article.new(article_params)
+     if @articles.save
+      redirect_to article_path(@articles)
+     else
+      render 'new'
+     end
+
   end
+
+  def show
+    @articles = Article.find(params[:id])
+
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
+
 
 
 
